d3cf633c6104d9d1948798b64a29c42b44c3d0d7
Syeda Hafsa <s.hafsa.5124@gmail.com>
2024-04-10 10:08:03 +0400

    ch 6 calculator
    javascript update

--- Chapter-6-Javascript/Exercises/Petrol Calculator (mandatory)/calculator.js
+++ Chapter-6-Javascript/Exercises/Petrol Calculator (mandatory)/calculator.js
@@ -3,5 +3,5 @@ function calculate () { //creating a function
     let num2 = document.getElementById ("litresPurchased").value; //creating a second variable for litres purchased
     let total = num1 * num2 // multiplying the two values adn storing it in a third variable
 
-    document.getElementById ("result").textContent = total; //chaning the result element to the total variable
+    document.getElementById ("result").textContent = total; //changing the result element to the total variable
 }
